ed88eeaf36fa64aca32e26ca2f16ee7511fa927c
ScottGreenhalgh <scottgreenhalgh62@me.com>
2024-09-09 17:13:04 +0100

    added content for seed file

---
+++ server/seed.js
@@ -0,0 +1,64 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = new pg.Pool({
+  connectionString: process.env.DB_URL,
+});
+
+const templateGame = [
+  "Counter Strike 2",
+  "Black Myth: Wukong",
+  "Dota 2",
+  "PUBG: BATTLEGROUNDS",
+  "Banana",
+  "Apex Legends",
+];
+
+const templateUsername = ["Bill", "Jeff", "Steve", "Tim", "Jimmy", "Connor"];
+
+const templateMessage = [
+  "Bill's Sample message",
+  "Jeff's Sample message",
+  "Steve's Sample message",
+  "Tim's Sample message",
+  "Jimmy's Sample message",
+  "Connor's Sample message",
+];
+
+const templateReview = [2, 4, 5, 5, 3, 1];
+
+const defaultLikes = 0;
+
+async function resetDb() {
+  await db.query("TRUNCATE comments RESTART IDENTITY");
+  for (let i = 0; templateUsername.length > i; i++) {
+    await db.query(
+      "INSERT INTO comments (game, username, message, reviewscore, likes, dislikes) VALUES ($1, $2, $3, $4, $5, $6)",
+      [
+        templateGame[i],
+        templateUsername[i],
+        templateMessage[i],
+        templateReview[i],
+        defaultLikes,
+        defaultLikes,
+      ]
+    );
+  }
+}
+
+resetDb();
+console.log("resetting database");
+
+// Table created with the following:
+
+// CREATE TABLE comments (
+//     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+//     game TEXT,
+//     username TEXT,
+//     message TEXT,
+//     reviewscore INT,
+//     likes INT,
+//     dislikes INT
+//   );
