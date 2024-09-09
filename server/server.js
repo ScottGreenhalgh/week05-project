5fd405533c349e1f2747da12a1bd22993ce8c0fe
Cxndr <livewellandmakethings@gmail.com>
2024-09-09 14:33:04 +0100

    basic tiktok api template

--- server/server.js
+++ server/server.js
@@ -1,33 +1,57 @@
-const tiktokClientKey = "sbaw8lr00nutwe3ayj";
-const tiktokClientSecret = "WBpuaSh06S2z5Hxs9n0keXzE9eB81wPo";
+import express from 'express';
+import cors from 'cors';
+import pg from 'pg';
+import dotenv from 'dotenv';
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
 
 let tiktokAuthToken;
 
-const setTiktokAuthToken = async () =>
-{
-    try 
-    {
-        const response = await fetch
-        (
-            "https://open.tiktokapis.com/v2/oauth/token/", 
-            {
-                method: "POST",
-                body: new URLSearchParams
-                ({
-                    "client_key": tiktokClientKey,
-                    "client_secret": tiktokClientSecret,
-                    "grant_type": "client_credentials"
-                })
-            }
-        );
+const setTiktokAuthToken = async () => {
+    try {
+        const response = await fetch("https://open.tiktokapis.com/v2/oauth/token/", {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Cache-Control": "no-cache"
+            },
+            method: "POST",
+            body: new URLSearchParams ({
+                "client_key": process.env.TIKTOK_CLIENT_KEY,
+                "client_secret": process.env.TIKTOK_CLIENT_SECRET,
+                "grant_type": "client_credentials"
+            })
+        });
         const responseJSON = await response.json();
-        tiktokAuthToken = responseJSON.access_token;
-        console.log(tiktokAuthToken);
+        console.log(responseJSON);
+        if (responseJSON.access_token) {
+            tiktokAuthToken = responseJSON.access_token;
+        }
     }
-    catch (err) 
-    {
+    catch (err) {
         console.error(err);
     }
 }
 
+const getTiktokData = async () => {
+    try {
+        let paramStr = "fields=id,like_count";
+        const url = "https://open.tiktokapis.com/v2/research/video/query/?" + new URLSearchParams(paramStr);
+        const response = await fetch(url,{
+            headers: {
+                "authorization": `Bearer ${tiktokAuthToken}`,
+            }
+        });
+        const responseJSON = await response.json();
+        let videoData = responseJSON.data;
+        console.log(videoData);
+    }
+    catch (err) {
+        console.log("failed getting data");
+        console.error(err);
+    }
+};
+
 setTiktokAuthToken();
+getTiktokData();
