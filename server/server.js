42a54fbc08764d01922cf7edec16e4050feb6a7b
Matt Vandersluys <matt.matiksaudio@gmail.com>
2024-09-10 09:39:37 +0100

    moved api code into api.js and added import into server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express, { response } from "express";
 import cors from "cors";
 import pg from "pg";
 import dotenv from "dotenv";
+import * as api from './api.js';
 
 const app = express();
 app.use(cors());
@@ -60,59 +61,6 @@ app.delete("/comments/:id", async function (request, response) {
   }
 });
 
-let tiktokAuthToken;
-
-const setTiktokAuthToken = async () => {
-  try {
-    const response = await fetch(
-      "https://open.tiktokapis.com/v2/oauth/token/",
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Cache-Control": "no-cache",
-        },
-        method: "POST",
-        body: new URLSearchParams({
-          client_key: process.env.TIKTOK_CLIENT_KEY,
-          client_secret: process.env.TIKTOK_CLIENT_SECRET,
-          grant_type: "client_credentials",
-        }),
-      }
-    );
-    const responseJSON = await response.json();
-    console.log(responseJSON);
-    if (responseJSON.access_token) {
-      tiktokAuthToken = responseJSON.access_token;
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-// ---------- API -----------
-
-const getTiktokData = async () => {
-  try {
-    let paramStr = "fields=id,like_count";
-    const url =
-      "https://open.tiktokapis.com/v2/research/video/query/?" +
-      new URLSearchParams(paramStr);
-    const response = await fetch(url, {
-      headers: {
-        authorization: `Bearer ${tiktokAuthToken}`,
-      },
-    });
-    const responseJSON = await response.json();
-    let videoData = responseJSON.data;
-    console.log(videoData);
-  } catch (err) {
-    console.log("failed getting data");
-    console.error(err);
-  }
-};
-
-setTiktokAuthToken();
-getTiktokData();
 
 // ---------- Start Server ------------
 
